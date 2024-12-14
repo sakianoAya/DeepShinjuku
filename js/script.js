8f1d12f38f1979c486181946a47c74e9053c4d44
sakianoaya <qwrr871210@gmail.com>
2024-12-14 11:04:47 +0900

    12/14 update

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                // 元素進入視窗，觸發滑入動畫
+                
                 entry.target.classList.add("slide-in");
             }
-            // 移除了 else 區塊，這樣元素離開視窗時就不會移除 slide-in class
+            
         });
     }, {
         threshold: 0.1
